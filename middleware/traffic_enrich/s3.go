dba6e98c1b159b27952af3a0af44471097ec0f46
Xiong Ding <dingxiong203@gmail.com>
2022-12-30 22:35:14 -0600

    abstract out the setting file

--- middleware/traffic_enrich/s3.go
+++ middleware/traffic_enrich/s3.go
@@ -1,24 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"net/textproto"
-	"os"
-	"strconv"
-	"encoding/json"
 	"strings"
 	"time"
 	"zip/infra/traffic_enrich/logs"
 
-	"github.com/buger/goreplay/proto"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/buger/goreplay/proto"
 )
 
 type httpRequest struct {
-	Path     []byte               `json:"path"`
+	Path    []byte               `json:"path"`
 	Method  []byte               `json:"method"`
 	Headers textproto.MIMEHeader `json:"headers"`
 	Body    []byte               `json:"body"`
@@ -26,41 +25,28 @@ type httpRequest struct {
 
 type s3Loader struct {
 	RequestsBuffer [][]byte
-	batchLoadSize  int
-	// client *s3.S3
-	bucket string
-	prefix string
-	// uploader *s3manager.Uploader
 }
 
 func newS3Loader() *s3Loader {
-	batchLoadSize := 1000
-	batchLoadSizeStr, found := os.LookupEnv("BATCH_LOAD_SIZE")
-	if found {
-		var err error
-		batchLoadSize, err = strconv.Atoi(batchLoadSizeStr)
-		if err != nil {
-			logs.Error("Fail to convert %s to integer.", batchLoadSizeStr)
-			panic(err)
-		}
-	}
-	loader := &s3Loader{RequestsBuffer: make([][]byte, 0), batchLoadSize: batchLoadSize}
+	loader := &s3Loader{RequestsBuffer: make([][]byte, 0)}
 	return loader
 }
 
 func (l *s3Loader) Enqueue(req []byte) {
 	l.RequestsBuffer = append(l.RequestsBuffer, req)
-	if len(l.RequestsBuffer) >= l.batchLoadSize {
+	if len(l.RequestsBuffer) >= AppSettings.S3BatchLoadSize {
 		requestsCopied := make([][]byte, len(l.RequestsBuffer))
 		copy(requestsCopied, l.RequestsBuffer)
 		l.RequestsBuffer = l.RequestsBuffer[:0] // clear the slice
-		go l.save(requestsCopied)
+		go upload(requestsCopied)
 	}
 }
 
-func (l *s3Loader) getS3uploader() *s3manager.Uploader {
+func getS3Uploader() *s3manager.Uploader {
 	session, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-west-2")},
+		Region:      aws.String(AppSettings.AwsRegion),
+		Credentials: credentials.NewStaticCredentials(AppSettings.AwsAccessKeyId, AppSettings.AwsSecretAccessKey, ""),
+	},
 	)
 	if err != nil {
 		logs.Fatal(err)
@@ -70,30 +56,35 @@ func (l *s3Loader) getS3uploader() *s3manager.Uploader {
 
 }
 
-func (l *s3Loader) save(requests [][]byte) {
+func upload(requests [][]byte) {
 	requestStrs := make([]string, 0)
-	for _, item := range(requests) {
-		r := httpRequest{Path: proto.Path(item), Method: proto.Method(item), Headers: proto.GetHeaders(item), Body: proto.Body(item)}
+	for _, item := range requests {
+		r := httpRequest{
+			Path:    proto.Path(item),
+			Method:  proto.Method(item),
+			Headers: proto.GetHeaders(item),
+			Body:    proto.Body(item),
+		}
 		s, err := json.Marshal(r)
 		if err != nil {
-			logs.Error("Fail to marshal request %v", r)
+			logs.Error("Fail to marshal request", r)
 			continue
 		}
 		requestStrs = append(requestStrs, string(s))
 	}
 	body := strings.Join(requestStrs, "\n")
 	t := time.Now().Format("20060102150405")
-	filename := fmt.Sprintf("%s/requests-%s-%d.txt", l.prefix, t, rand.Intn(1000))
+	filename := fmt.Sprintf("%s/requests-%s-%d.txt", AppSettings.S3BucketPrefix, t, rand.Intn(1000))
 
-	uploader := l.getS3uploader()
+	uploader := getS3Uploader()
 	_, err := uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String(l.bucket),
+		Bucket: aws.String(AppSettings.S3BucketName),
 		Key:    aws.String(filename),
 		Body:   strings.NewReader(body),
 	})
 	if err != nil {
-		logs.Error("Unable to upload %q to %q, %v", filename, l.bucket, err)
+		logs.Error("Unable to upload", filename, "to", AppSettings.S3BucketName, ". error is", err)
 	} else {
-		logs.Info("Successfully uploaded %d to %q\n", filename, l.bucket)
+		logs.Info("Successfully uploaded", filename, "to", AppSettings.S3BucketName)
 	}
 }
---
+++ middleware/traffic_enrich/settings.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"os"
+	"strconv"
+
+	"zip/infra/traffic_enrich/logs"
+)
+
+// Settings are a set of configurations loaded from environment variables.
+type Settings struct {
+	AwsRegion          string
+	AwsAccessKeyId     string
+	AwsSecretAccessKey string
+	S3BucketName       string
+	S3BucketPrefix     string
+	S3BatchLoadSize    int
+}
+
+func lookupEnvOrDefault(key string, defaultValue string) string {
+	v, found := os.LookupEnv(key)
+	if found {
+		return v
+	}
+	return defaultValue
+}
+
+func lookupEnvOrExit(key string) string {
+	v, found := os.LookupEnv(key)
+	if !found {
+		logs.Fatal("Environment variable", key, "does not exits.")
+	}
+	return v
+}
+
+func newSettings() *Settings {
+	st := &Settings{}
+
+	st.AwsRegion = lookupEnvOrDefault("AWS_REGION", "us-east-2")
+	st.AwsAccessKeyId = lookupEnvOrExit("TRAFFIC_REPLAY_AWS_ACCESS_KEY_ID")
+	st.AwsSecretAccessKey = lookupEnvOrExit("TRAFFIC_REPLAY_AWS_SECRET_ACCESS_KEY")
+
+	st.S3BucketName = lookupEnvOrDefault("TRAFFIC_REPLAY_BUCKET_NAME", "zip-traffic-replay")
+	st.S3BucketPrefix = lookupEnvOrDefault("TRAFFIC_REPLAY_BUCKET_PREFIX", "test")
+
+	batchLoadSizeStr := lookupEnvOrDefault("BATCH_LOAD_SIZE", "1000")
+	batchLoadSize, err := strconv.Atoi(batchLoadSizeStr)
+	if err != nil {
+		logs.Fatal("Fail to convert", batchLoadSize, "to integer.")
+	}
+	st.S3BatchLoadSize = batchLoadSize
+
+	return st
+}
+
+var AppSettings *Settings
+
+func init() {
+	AppSettings = newSettings()
+}
